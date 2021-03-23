988d5b32162a5d8a011bd06942cf617c7e68cd3b
AbhinavRy <royabhinav238@gmail.com>
2021-03-23 19:54:39 +0530

    storage with validation

--- schemeRoute.js
+++ schemeRoute.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const request = require("request-promise");
 const cheerio = require("cheerio");
+const WhichX  = require("whichx");
+const whichpet = new WhichX();
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const Schemes = require("./models/schemesModel");
 
 const urlWithId = `https://services.india.gov.in/service/listing?cat_id=1&ln=en`;
+
+const categories = ["education","health","finance","employment"];
+
+whichpet.addLabels(categories);
+whichpet.addData("education","education teach teaching teacher student school scholar scholars");
+whichpet.addData("health","health medical medicine hospital healthcare disease medical treatment");
+whichpet.addData("finance","payment tax education loan finance financial scholarship aid dhan bima mudra financial assistance");
+whichpet.addData("employment","employment training employer skill skills employee employ ")
 
 router.get("/",
     async (req,res) => {
@@ -37,13 +47,16 @@ router.post("/",
                     if(divs_h3 && divs_a){
                         const name = $(divs_h3).text().trim();
                         const link = $(divs_a).attr("href").trim();
-                        arrayOfSchemes.push(name,link);
+                        var schemeCategory = whichpet.classify(name);
+                        schemeCategory = !schemeCategory?"No Category":schemeCategory;
+                        arrayOfSchemes.push(name,link,schemeCategory);
                         try {
-                            const existingScheme = await Schemes.find({name:name});
-                            if(!existingScheme && name && link){
+                            let existingScheme = await Schemes.find({name:name});
+                            if(!existingScheme[0] && (name && link)){
                                 scheme = new Schemes({
                                     name:name,
-                                    link:link
+                                    link:link,
+                                    category:schemeCategory
                                 });
                                 await scheme.save();
                             }
