f805e8b52826ba7a4dc46388f4c5ec6256e75fea
AbhinavRy <royabhinav238@gmail.com>
2021-03-24 02:49:38 +0530

    php form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import App from './App';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route exact path='/' component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
